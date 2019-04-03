a797ba70e4016b59510a2c3a33c4b5897506522f
Martin Ponce <doc.brown.83@gmail.com>
2019-04-03 20:06:30 +0800

    Implement qualification rules (#6)

--- honours-server/src/test/java/au/com/martinponce/honours/server/HonoursEngineAssessTest.java
+++ honours-server/src/test/java/au/com/martinponce/honours/server/HonoursEngineAssessTest.java
@@ -7,35 +7,11 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Honours engine assessment test")
 class HonoursEngineAssessTest {
-
-  @Test
-  @DisplayName("Disqualified with too many fails")
-  void isDisqualifiedTooManyFails() {
-    HonoursEngine sut = new HonoursEngine();
-    List<Integer> marks = IntStream.range(1, HonoursEngine.MIN_MARKS)
-        .boxed()
-        .map(i -> HonoursEngine.PASS_MARK - 1)
-        .collect(Collectors.toList());
-    assertTrue(sut.isDisqualified(marks));
-  }
-
-  @Test
-  @DisplayName("Not disqualified with only one fail")
-  void notDisqualifiedOneFail() {
-    HonoursEngine sut = new HonoursEngine();
-    List<Integer> marks = IntStream.range(1, HonoursEngine.MIN_MARKS)
-        .boxed()
-        .map(i -> HonoursEngine.PASS_MARK)
-        .collect(Collectors.toList());
-    marks.add(HonoursEngine.PASS_MARK - 1);
-    assertFalse(sut.isDisqualified(marks));
-  }
 
   @Test
   @DisplayName("Calculate mark average")
@@ -61,5 +37,75 @@ class HonoursEngineAssessTest {
         .sorted(Comparator.reverseOrder())
         .collect(Collectors.toList());
     assertEquals(expected, sut.top(marks, limit).collect(Collectors.toList()));
+  }
+
+  @Test
+  @DisplayName("Assess denied")
+  void assessDenied() {
+    List<Integer> marks = IntStream.rangeClosed(1, HonoursEngine.MAX_FAILS + 1)
+        .boxed()
+        .map(i -> HonoursEngine.PASS_MARK - 1)
+        .collect(Collectors.toList());
+    HonoursEngine sut = new HonoursEngine();
+    assertEquals(Result.DENIED, sut.assess(marks));
+  }
+
+  @Test
+  @DisplayName("Assess qualified")
+  void assessQualified() {
+    List<Integer> marks = IntStream.rangeClosed(1, 3)
+        .boxed()
+        .map(i -> (int) HonoursEngine.AVG_FOR_QUALIFY)
+        .collect(Collectors.toList());
+    HonoursEngine sut = new HonoursEngine();
+    assertEquals(Result.QUALIFIED, sut.assess(marks));
+  }
+
+  @Test
+  @DisplayName("Assess need assessment")
+  void assessNeedAssessment() {
+    List<Integer> topMarks = IntStream.rangeClosed(1, 8)
+        .boxed()
+        .map(i -> (int) HonoursEngine.AVG_FOR_ASSESSMENT)
+        .collect(Collectors.toList());
+    List<Integer> marks = IntStream.rangeClosed(1, 22)
+        .boxed()
+        .map(i -> HonoursEngine.PASS_MARK)
+        .collect(Collectors.toList());
+    marks.addAll(topMarks);
+    HonoursEngine sut = new HonoursEngine();
+    assertEquals(Result.NEED_ASSESSMENT, sut.assess(marks));
+  }
+
+  @Test
+  @DisplayName("Assess need permission")
+  void assessNeedPermission() {
+    List<Integer> topMarks = IntStream.rangeClosed(1, 8)
+        .boxed()
+        .map(i -> (int) HonoursEngine.AVG_FOR_PERMISSION)
+        .collect(Collectors.toList());
+    List<Integer> marks = IntStream.rangeClosed(1, 22)
+        .boxed()
+        .map(i -> HonoursEngine.PASS_MARK)
+        .collect(Collectors.toList());
+    marks.addAll(topMarks);
+    HonoursEngine sut = new HonoursEngine();
+    assertEquals(Result.NEED_PERMISSION, sut.assess(marks));
+  }
+
+  @Test
+  @DisplayName("Assess need permission")
+  void assessNotQualified() {
+    List<Integer> topMarks = IntStream.rangeClosed(1, 8)
+        .boxed()
+        .map(i -> (int) HonoursEngine.AVG_FOR_PERMISSION - 1)
+        .collect(Collectors.toList());
+    List<Integer> marks = IntStream.rangeClosed(1, 22)
+        .boxed()
+        .map(i -> HonoursEngine.PASS_MARK)
+        .collect(Collectors.toList());
+    marks.addAll(topMarks);
+    HonoursEngine sut = new HonoursEngine();
+    assertEquals(Result.NOT_QUALIFIED, sut.assess(marks));
   }
 }

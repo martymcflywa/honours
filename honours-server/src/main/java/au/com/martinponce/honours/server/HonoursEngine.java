f0bff7cd229c903fa90421aa8280d01b271c2b26
Martin Ponce <martin.ponce@outlook.com>
2019-04-03 20:01:25 +0800

    Implement qualification rules

--- honours-server/src/main/java/au/com/martinponce/honours/server/HonoursEngine.java
+++ honours-server/src/main/java/au/com/martinponce/honours/server/HonoursEngine.java
@@ -14,9 +14,13 @@ public class HonoursEngine implements IAssess {
 
   static final int MIN_MARKS = 12;
   static final int MAX_MARKS = 30;
-  static final int MAX_FAILS = 5;
-
   static final int PASS_MARK = 50;
+  static final int MAX_FAILS = 5;
+  static final double AVG_FOR_QUALIFY = 70d;
+  static final double AVG_FOR_ASSESSMENT = 80d;
+  static final double AVG_FOR_PERMISSION = 70d;
+
+  private static final int TOP_COUNT = 8;
 
   /**
    * @param id The student's id.
@@ -53,25 +57,16 @@ public class HonoursEngine implements IAssess {
   }
 
   /**
-   * Determine if student is disqualified for honours study.
-   * Cannot have more than MAX_FAIL failed/incomplete units.
-   * @param marks The course unit marks.
-   * @return true if failed/incomplete more than MAX_FAIL units.
-   */
-  boolean isDisqualified(Collection<Integer> marks) {
-    return  marks.stream()
-        .filter(i -> i < PASS_MARK)
-        .count() > MAX_FAILS;
-  }
-
-  /**
    * Calculate average of submitted course marks.
    * @param marks The course unit marks.
    * @return average of submitted course marks, at 2 decimal places.
    */
   double average(Collection<Integer> marks) {
-    double average = marks.stream()
-        .mapToInt(i -> i)
+    return average(marks.stream());
+  }
+
+  private double average(Stream<Integer> marks) {
+    double average = marks.mapToInt(i -> i)
         .average()
         .orElseThrow(() -> new NullPointerException("Mark average is null"));
     return new BigDecimal(average)
@@ -89,5 +84,68 @@ public class HonoursEngine implements IAssess {
     return marks.stream()
         .sorted(Comparator.reverseOrder())
         .limit(n);
+  }
+
+  /**
+   * Returns the appropriate assessment result, based on the total average,
+   * and top marks average.
+   * @param marks The course unit marks
+   * @return The result.
+   */
+  Result assess(Collection<Integer> marks) {
+    double average = average(marks);
+    double topEightAverage = average(top(marks, TOP_COUNT));
+
+    if (isDenied(marks))
+      return Result.DENIED;
+    if (isQualified(average))
+      return Result.QUALIFIED;
+    if (isAssessmentRequired(topEightAverage))
+      return Result.NEED_ASSESSMENT;
+    if (isPermissionRequired(topEightAverage))
+      return Result.NEED_PERMISSION;
+
+    return Result.NOT_QUALIFIED;
+  }
+
+  /**
+   * Determine if student is denied for honours study.
+   * Cannot have more than MAX_FAIL failed/incomplete units.
+   * @param marks The course unit marks.
+   * @return true if failed/incomplete more than MAX_FAIL units.
+   */
+  private boolean isDenied(Collection<Integer> marks) {
+    return marks.stream()
+        .filter(i -> i < PASS_MARK)
+        .count() > MAX_FAILS;
+  }
+
+  /**
+   * Determine if student is qualified for honours study.
+   * @param average The overall average.
+   * @return true if average is greater than or equal to AVG_FOR_QUALIFY.
+   */
+  private boolean isQualified(double average) {
+    return average >= AVG_FOR_QUALIFY;
+  }
+
+  /**
+   * Determine if student requires further assessment for honours study.
+   * @param average The top eight highest scoring units average.
+   * @return true if top eight highest scoring units average is higher than
+   * AVG_FOR_ASSESSMENT.
+   */
+  private boolean isAssessmentRequired(double average) {
+    return average >= AVG_FOR_ASSESSMENT;
+  }
+
+  /**
+   * Determine if student requires special permission for honours study.
+   * @param average The top eight highest scoring units average.
+   * @return true if top eight highest scoring units average is higher than
+   * AVG_FOR_PERMISSION.
+   */
+  private boolean isPermissionRequired(double average) {
+    return average >= AVG_FOR_PERMISSION;
   }
 }
---
+++ honours-server/src/main/java/au/com/martinponce/honours/server/Result.java
@@ -0,0 +1,9 @@
+package au.com.martinponce.honours.server;
+
+public enum Result {
+  QUALIFIED,
+  NEED_ASSESSMENT,
+  NEED_PERMISSION,
+  NOT_QUALIFIED,
+  DENIED
+}

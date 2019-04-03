a53d6de752d66df35523ffd84f9e7b74cc24f049
Martin Ponce <doc.brown.83@gmail.com>
2019-04-03 14:42:54 +0800

    Server-side interface parameter validation (#3)

---
+++ honours-server/src/main/java/au/com/martinponce/honours/server/HonoursEngine.java
@@ -0,0 +1,28 @@
+package au.com.martinponce.honours.server;
+
+import au.com.martinponce.honours.interfaces.IAssess;
+import org.apache.commons.lang3.Validate;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+public class HonoursEngine implements IAssess {
+
+  @Override
+  public String assess(String id, List<Integer> marks) throws RemoteException {
+
+    return null;
+  }
+
+  void validateId(String id) {
+    Validate.notEmpty(id);
+  }
+
+  void validateMarks(List<Integer> marks) {
+    int min = 12;
+    int max = 30;
+    Validate.notEmpty(marks);
+    Validate.isTrue(marks.size() >= min, "Min %d marks", min);
+    Validate.isTrue(marks.size() < max, "Max %d marks", max);
+  }
+}
